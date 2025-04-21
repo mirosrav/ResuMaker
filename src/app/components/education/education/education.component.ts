befc21e5ce794f00fc4a964fcd7bfd53be10f32c
Aizat Kamarudin <aizat.ash@gmail.com>
2025-04-21 16:33:07 +0800

    Added functionality for education component

--- src/app/components/education/education/education.component.ts
+++ src/app/components/education/education/education.component.ts
@@ -1,13 +1,73 @@
-import { Component } from '@angular/core';
-import { EducationListComponent } from "./education-list/education-list.component";
+import { Component, OnInit } from '@angular/core';
+import { SharedServiceService } from '../../../services/sharedService.service';
+import { FormArray, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-education',
   standalone: true,
-  imports: [EducationListComponent],
+  imports: [ReactiveFormsModule,CommonModule],
   templateUrl: './education.component.html',
   styleUrl: './education.component.css'
 })
-export class EducationComponent {
+export class EducationComponent implements OnInit{
+  hasDataInLocalStorage:boolean = false;
+
+  eduStorageData = JSON.parse(localStorage.getItem('eduData') || '{}');
+
+  constructor(private eduData:SharedServiceService){
+
+  }
+
+  ngOnInit(){
+    this.hasDataInLocalStorage = !!this.eduStorageData ;
+    const savedData = this.eduStorageData;
+    if(savedData?.educations?.length){
+      savedData.educations.forEach((edu:any)=>{
+        this.educations.push(this.createEducationList(edu));
+      });
+    }else{
+      this.educations.push(this.createEducationList());
+    }
+
+    this.educationListForm.valueChanges.subscribe((newdata)=>{
+      this.eduData.updateEduPreview(newdata);
+    })
+  }
+
+  educationListForm = new FormGroup({
+    educations: new FormArray([])
+  })
+
+  createEducationList(data?:any):FormGroup{
+    return new FormGroup({
+      title: new FormControl(data?.title || '',Validators.required),
+      date: new FormControl(data?.date || '',Validators.required),
+      institute: new FormControl(data?.institute || '', Validators.required)
+    })
+  }
+
+  get educations():FormArray{
+    return this.educationListForm.get('educations') as FormArray;
+  }
+
+  addEducation(){
+    this.educations.push(this.createEducationList());
+  }
+
+  updateEducation(){
+    this.eduData.updateEduForm(this.educationListForm.value);
+    // this.eduData.nextStep();
+  }
+
+  deleteEducation(index:number){
+    const confirmed = confirm("Are you sure to delete this education?")
+    if(confirmed){
+      this.educations.removeAt(index);
+    }
+  }
+
+
+
 
 }
