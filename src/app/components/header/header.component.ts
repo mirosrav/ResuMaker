7c6666b2ffaa8f436a56ba5b7dbc5f626b23e9d8
Aizat Kamarudin <aizat.ash@gmail.com>
2025-05-08 16:34:05 +0800

    Added validators to each formcontrols

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,54 +1,69 @@
-import { Component, Input, Output,EventEmitter, OnInit } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 // import { EventEmitter } from 'stream';
 import { SharedServiceService } from '../../services/sharedService.service';
 import { __values } from 'tslib';
+import { FormErrorComponent } from '../../shared/form-error/form-error.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, FormErrorComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
-export class HeaderComponent implements OnInit{
+export class HeaderComponent implements OnInit {
   isHeaderVisible = true;
   @Output() next = new EventEmitter<void>();
   headerStorageData = JSON.parse(localStorage.getItem('headerData') || '{}');
-  constructor(private headerDataService: SharedServiceService){
+  constructor(private headerDataService: SharedServiceService) {
 
   }
 
-  ngOnInit(){
+  ngOnInit() {
     const savedData = this.headerDataService.headerFormData();
-    if(savedData){
+    if (savedData) {
       this.headerForm.patchValue(savedData);
     }
 
-    this.headerForm.valueChanges.subscribe((newData)=>{
+    this.headerForm.valueChanges.subscribe((newData) => {
       this.headerDataService.updateHeaderPreview(newData);
     })
   }
 
   headerForm = new FormGroup({
-    firstName: new FormControl(''),
-    lastName: new FormControl(''),
+    firstName: new FormControl('', Validators.required),
+    lastName: new FormControl('', Validators.required),
     contact: new FormGroup({
-      location: new FormControl(''),
-      email: new FormControl(''),
-      phone: new FormControl(''),
+      location: new FormControl('', Validators.required),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      phone: new FormControl('', Validators.required),
     }),
   })
 
-  updateForm(e:Event){
+  updateForm(e: Event) {
     e.preventDefault();
+    this.markFormGroupTouched(this.headerForm);
+    
+    if (this.headerForm.invalid) {
+      return;
+    }
+    
     this.headerDataService.updateHeaderForm(this.headerForm.value);
-    this.proceed();
-  }
-
-  proceed(){
     this.headerDataService.nextStep();
   }
-}  
+
+  markFormGroupTouched(formGroup: FormGroup) {
+  Object.values(formGroup.controls).forEach(control => {
+    if (control instanceof FormGroup) {
+      this.markFormGroupTouched(control); // recursively touch nested groups
+    } else {
+      control.markAsTouched();
+    }
+  });
+}
+
+
+}
 
 
