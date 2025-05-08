56112824871dd05119182d13c642a7e6f86be94d
Aizat Kamarudin <aizat.ash@gmail.com>
2025-05-08 16:32:46 +0800

    updated formatting & updated the date input type to date and splitted the date input field to start and end

--- src/app/components/experience/experience-list/experience-list.component.ts
+++ src/app/components/experience/experience-list/experience-list.component.ts
@@ -11,85 +11,94 @@ import { FormErrorComponent } from '../../../shared/form-error/form-error.compon
   templateUrl: './experience-list.component.html',
   styleUrl: './experience-list.component.css'
 })
-export class ExperienceListComponent implements OnInit{
-  expStorageData =  JSON.parse(localStorage.getItem('expData') || '{}');
+export class ExperienceListComponent implements OnInit {
+  expStorageData = JSON.parse(localStorage.getItem('expData') || '{}');
 
-  constructor(private expData:SharedServiceService){
+  constructor(private expDataService: SharedServiceService) {
   }
 
   ngOnInit() {
     const savedData = this.expStorageData;
-    if(savedData?.experiences?.length){
-      savedData.experiences.forEach((exp:any) => {
+    if (savedData?.experiences?.length) {
+      savedData.experiences.forEach((exp: any) => {
         this.experiences.push(this.createExperienceList(exp));
       });
-    }else{
+    } else {
       this.experiences.push(this.createExperienceList());
     }
-    
-  
+
     this.experienceListForm.valueChanges.subscribe((newData) => {
-      this.expData.updateExpPreview(newData);
+      this.expDataService.updateExpPreview(newData);
     });
   }
-
 
   experienceListForm = new FormGroup({
     experiences: new FormArray([])
   })
 
-  createExperienceList(data?:any):FormGroup{
+  createExperienceList(data?: any): FormGroup {
     return new FormGroup({
-      company:new FormControl(data?.company || '', Validators.required),
-      designation:new FormControl(data?.designation || '', Validators.required),
-      date: new FormControl(data?.date || '', Validators.required),
+      company: new FormControl(data?.company || '', Validators.required),
+      designation: new FormControl(data?.designation || '', Validators.required),
+      startDate: new FormControl(data?.startDate || '', Validators.required),
+      endDate: new FormControl(data?.endDate || '', Validators.required),
       compLocation: new FormControl(data?.compLocation || '', Validators.required),
-      expHighlight : new FormArray(
+      expHighlight: new FormArray(
         (data?.expHighlight?.length ? data.expHighlight : ['']).map(
-          (hl:string) => new FormControl(hl, Validators.required)
+          (hl: string) => new FormControl(hl, Validators.required)
         ))
     });
   }
 
-  get experiences():FormArray{
+  get experiences(): FormArray {
     return this.experienceListForm.get('experiences') as FormArray;//get formArray experiences
   }
 
-  getHighlights(index:number):FormArray{
+  getHighlights(index: number): FormArray {
     return this.experiences.at(index).get('expHighlight') as FormArray;//get formArray highlight
   }
 
-  addHighlight(index:number){
+  addHighlight(index: number) {
     this.getHighlights(index).push(new FormControl(''));
   }
 
-  addExperience(){
+  addExperience() {
     this.experiences?.push(this.createExperienceList());
   }
 
-  updateExperience(){
-    this.expData.updateExpForm(this.experienceListForm.value);
-    // this.expData.nextStep();
-  }
-
-  deleteHighlight(index:number,highlightIndex:number){
+  deleteHighlight(index: number, highlightIndex: number) {
     const confirmed = confirm("Are you sure to delete this work highlight?")
-    if(confirmed){
+    if (confirmed) {
       this.getHighlights(index).removeAt(highlightIndex);
     }
   }
 
-  deleteExperience(index:number){
+  deleteExperience(index: number) {
     this.experiences.removeAt(index);
   }
 
-  next(){
-    this.expData.nextStep();
-    this.updateExperience();
+  next() {
+    this.markFormTouched(this.experienceListForm);
+    if (this.experienceListForm.invalid) {
+      return;
+    }
+
+    this.expDataService.nextStep();
+    this.expDataService.updateExpForm(this.experienceListForm.value);
   }
 
-  previous(){
-    this.expData.prevStep();
+  previous() {
+    this.expDataService.prevStep();
+  }
+
+  markFormTouched(group: FormGroup | FormArray) {
+    Object.values(group.controls).forEach(control => {
+      if (control instanceof FormGroup || control instanceof FormArray) {
+        this.markFormTouched(control);
+      } else {
+        control.markAsTouched();
+      }
+    })
   }
 
 }
